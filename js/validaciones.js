cfd7817a022359fb1f1dd583b3dc8c943f544389
Diego Alexander <diegoa.ipialeso@uqvirtual.edu.co>
2021-10-09 14:09:32 -0500

    actualizacion archivos

---
+++ js/validaciones.js
@@ -0,0 +1,18 @@
+
+
+function validar_nombreUsuario(String){
+
+
+
+
+}
+
+
+function validar_contrasena(String){
+
+}
+
+
+module.exports.validar_nombreUsuario = validar_nombreUsuario;
+module.exports.validar_contrasena = validar_contrasena;
+
