0c7fd84d8d789319f9e6a02a097a481c3d1dcf79
Diego Alexander <diegoa.ipialeso@uqvirtual.edu.co>
2021-10-13 08:06:35 -0500

    Se crea archivo validaciones.js

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,16 +1,20 @@
 
 
 function validar_nombreUsuario(String){
-
-
-
-
-}
-
+   let input = document.getElementById("in_usuario");
+   let regxp = /^([a-zA-Z0-9_-]){3,9}$/;
+      if(input != regxp){
+      return(regxp.test(input.value));
+      }
+   }
 
 function validar_contrasena(String){
-
-}
+   let input = document.getElementById("in_contrasena");
+   let regxp = /^([a-zA-Z0-9_-]){6,20}$/;
+      if(input != regxp){
+      return(regxp.test(input.value));
+      }
+   }
 
 
 module.exports.validar_nombreUsuario = validar_nombreUsuario;
